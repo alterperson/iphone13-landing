4aa8a808fff8388becaadbad925d8ced28c69dc4
alterperson <vad.mk@ya.ru>
2021-08-22 13:07:31 +0300

    Отправка формы AJAX
    Подключение Jquery + Код для отправки формы на AJAX

--- js/script.js
+++ js/script.js
@@ -7,3 +7,19 @@ new Choices("#color", {
   searchEnabled: false,
   itemSelectText: "",
 });
+
+$("#form-order").on("submit", function (event) {
+  event.preventDefault();
+
+  $.ajax({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    method: "POST",
+    dataType: "html",
+    data: $("#form-order").serialize(),
+    success: function (data) {
+      console.log(data);
+      $("#form-order").html("Спасибо! Ваша заявка отправлена!");
+      $(".form__submit").hide();
+    },
+  });
+});
